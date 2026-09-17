7e861a99013f6aedd0c8cb1ce3bb2246623437d9
alons126 <alons126@gmail.com>
2026-09-17 10:53:08 +0300

    Remove legacy uniform sample generator files and associated code
    - Deleted Histograms.h, Uniform_sample_generator.C, Uniform_sample_generator_e_tester.C, addParticle.C, main.cpp, run.sh, and targets.h files.
    - These files were part of the legacy uniform sample generator and are no longer needed in the current codebase.

--- legacy/Uniform-sample-generator/BoolToString.C
+++
@@ -1,1 +0,0 @@
-inline const char *const BoolToString(bool b) { return b ? "true" : "false"; }
--- legacy/Uniform-sample-generator/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Uniform_sample_generator.C"
-
-int main() {
-    Uniform_sample_generator();
-    return 0;
-}
