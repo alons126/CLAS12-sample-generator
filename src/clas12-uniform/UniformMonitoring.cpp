d72270bab0435115f18c09364f7e5c1409eec9f2
alons126 <alons126@gmail.com>
2026-09-19 12:59:08 +0300

    Increase canvas size for improved visualization in uniform monitoring output

--- src/clas12-uniform/UniformMonitoring.cpp
+++ src/clas12-uniform/UniformMonitoring.cpp
@@ -254,7 +254,7 @@ void UniformMonitoring::save(const std::filesystem::path& path, const std::files
     gROOT->SetBatch(true);
     std::filesystem::create_directories(plot_directory);
     const auto pdf = (plot_directory / pdf_name).string();
-    TCanvas canvas("canvas", "canvas", 1000, 750);
+    TCanvas canvas("canvas", "canvas", 1000 * 2, 750 * 2);
     canvas.SetGrid();
     canvas.SetBottomMargin(0.14);
     canvas.SetLeftMargin(0.16);
